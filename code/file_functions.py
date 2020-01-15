cbd84d7fb2e65bae264532c3f8a3eccbf764e92a
popenyuk <zenovij.popenyuk@gmail.com>
2020-01-15 10:57:45 +0200

    Added comments to functions.py

--- code/file_functions.py
+++ code/file_functions.py
@@ -3,18 +3,39 @@ import pandas as pd
 
 
 def check_if_file_exist(name_of_file):
+    """
+    Check if a file with path name_of_file exists
+    :param name_of_file: String
+    :return: bool
+    """
     return os.path.isfile(name_of_file)
 
 
 def check_if_directory_exist(name_of_directory):
+    """
+    Check if a directory with path name_of_directory exists
+    :param name_of_directory: String
+    :return: bool
+    """
     return os.path.exists(name_of_directory)
 
 
 def create_directory(name_of_directory):
+    """
+    Create directory with provided path
+    :param name_of_directory: String
+    :return: None
+    """
     os.mkdir(name_of_directory)
 
 
 def get_dataset_from_internet(user_data_dataset_name, song_data_dataset_name):
+    """
+    Download provided datasets and merge it in one.
+    :param user_data_dataset_name: String
+    :param song_data_dataset_name: String
+    :return: pandas.DataFrame
+    """
     user_df = pd.read_table(user_data_dataset_name, header=None)
     user_df.columns = ['user_id', 'song_id', 'listen_count']
     song_df = pd.read_csv(song_data_dataset_name)
@@ -24,6 +45,15 @@ def get_dataset_from_internet(user_data_dataset_name, song_data_dataset_name):
 
 def load_dataset(dataset_directory, user_data_dataset_name,
                  song_data_dataset_name, dataset_filename):
+    """
+    We check that the data files are downloaded, if not downloadedб then
+    and saved to a file, if downloaded, then just read from the file.
+    :param dataset_directory: String
+    :param user_data_dataset_name: String
+    :param song_data_dataset_name: String
+    :param dataset_filename: String
+    :return: pandas.DataFrame
+    """
     if not check_if_directory_exist(dataset_directory):
         create_directory(dataset_directory)
 
